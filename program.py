803cc845955f3ce7d1804d32379e6b73b6cccdd0
paninpv <paninpv@yandexlyceum.ru>
2024-01-22 14:57:08 +0500

    '12333'

--- program.py
+++ program.py
@@ -1,7 +1,7 @@
 def main():
     # Use a breakpoint in the code line below to debug your script.
     print('My first git program-----')  # Press Ctrl+F8 to toggle the breakpoint.
-    print('Privet')
+    print('Privet____')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
