b31fab8070dee247e4be719fb39ebcc70924d3f0
paninpv <paninpv@yandexlyceum.ru>
2024-01-22 13:36:45 +0500

    "First Commit"

---
+++ program.py
@@ -0,0 +1,8 @@
+def main():
+    # Use a breakpoint in the code line below to debug your script.
+    print('My first git program-----')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
